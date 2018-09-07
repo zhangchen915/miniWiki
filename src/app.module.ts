4f3ae3735bdd356ac6414eac83d73443862bebe5
Chen <zhangchen915@outlook.com>
2018-09-07 22:52:59 +0800

    add: PayloadMiddleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { RegisterModule } from './register/register.module';
+import { PayloadMiddleware } from './middlewares/payload.middleware';
 
 @Module({
   imports: [TypeOrmModule.forRoot(), UsersModule, AuthModule, RegisterModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(PayloadMiddleware)
+      .forRoutes('auth');
+  }
 }
---
+++ src/middlewares/payload.middleware.ts
@@ -0,0 +1,24 @@
+import { Injectable, NestMiddleware, MiddlewareFunction } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class PayloadMiddleware implements NestMiddleware {
+  constructor(
+    private readonly jwtService: JwtService,
+  ) {
+  }
+
+  resolve(...args: any[]): MiddlewareFunction {
+    return (req, res, next) => {
+      if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+        const token = req.headers.authorization.split(' ')[1];
+        req.payload = this.jwtService.verify(token);
+        next();
+      }
+      else {
+        next();
+        // return res.status(401).json('The access token is not valid.');
+      }
+    };
+  }
+}
