e88af9eb166723b498b54ec104f40901538bf93a
Chen <zhangchen915@outlook.com>
2018-09-09 20:35:41 +0800

    add: handel token expired

--- src/middlewares/payload.middleware.ts
+++ src/middlewares/payload.middleware.ts
@@ -1,4 +1,5 @@
 import { Injectable, NestMiddleware, MiddlewareFunction } from '@nestjs/common';
+import { TokenExpiredError } from 'jsonwebtoken';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
@@ -12,9 +13,20 @@ export class PayloadMiddleware implements NestMiddleware {
     return (req, res, next) => {
       if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
         const token = req.headers.authorization.split(' ')[1];
-        req.payload = this.jwtService.verify(token);
+        req.payload = this.jwtService.decode(token, { json: true });
+        try {
+          this.jwtService.verify(token);
+          next();
+        } catch (e) {
+          if (e instanceof TokenExpiredError) {
+            const payload = Object.assign({}, req.payload);
+            delete payload.exp;
+            delete payload.nbf;
+            res.set('Authorization', this.jwtService.sign(payload));
+            next();
+          }
+        }
       }
-      next();
     };
   }
 }
